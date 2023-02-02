94070f3676b7af974e0996d125a21965c417e08d
Your Name <miraaqib514@gmail.com>
2023-02-02 12:47:12 +0530

    First redux

---
+++ Redux.js
@@ -0,0 +1,30 @@
+const redux = require('redux');
+
+const counterReducer = (state = {counter:0}, action) => {
+    if(action.type==='increment'){
+        return {
+            counter:state.counter + 1
+        };
+    }
+    if(action.type==='decrement'){
+        return {
+            counter:state.counter-5
+        }
+    }
+    return state;
+   
+};
+
+
+const store = redux.createStore(counterReducer);
+console.log(store.getState());
+const counterSubscriber = ()=>{
+
+    //return the lastes state
+    const latestState =  store.getState();
+    console.log(latestState)
+}
+store.subscribe(counterSubscriber);
+
+store.dispatch({type:'increment'});
+store.dispatch({type:'decrement'})
